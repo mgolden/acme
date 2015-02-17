daf1cac201e23c2845341e0e1871995fa7b44e3c
Mitch Golden <mgolden@mitchgolden.com>
2015-02-17 11:10:47 -0500

    Added special boxes

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -8,8 +8,14 @@
 #endif
 
 extrn thing *stack;
-extrn box_list *b_true, *b_false, *b_nil, *b_i, *b_f, *b_s, *b_sym;
+extrn box_list *b_true, *b_false, *b_nil, *b_i, *b_f, *b_s, *b_sym, *b_hash, *b_array;
 extrn thing *t_true, *t_false, *t_nil;
 extrn int sp;
+
+#ifdef MAIN
+extern box_list *special_boxes[];
+else
+box_list *special_boxes[] = {b_true, b_false, b_nil, b_i, b_f, b_s, b_sym, b_hash, b_array};
+#endif
 
 #endif
